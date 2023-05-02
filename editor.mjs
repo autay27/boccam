3587b8a305066b915f35b6f2df92d0b9b169fad3
autay27 <au.tay.27@gmail.com>
2023-05-02 17:50:01 +0100

    got codemirror editor working  and with indenting

--- editor.mjs
+++ editor.mjs
@@ -3,62 +3,42 @@ import {EditorView, keymap} from "@codemirror/view"
 import {indentWithTab} from "@codemirror/commands"
 import {javascript} from "@codemirror/lang-javascript"
 
-const doc = `if (true) {
-  console.log("okay")
-} else {
-  console.log("oh no")
-}
+const exampleCode = `SEQ
+    CHAN OF INT chan:
+    INT x:
+    INT y:
+    x := 0
+    PAR
+        WHILE TRUE
+            SEQ
+                chan ! x
+                x := (x+1)
+        WHILE TRUE
+            SEQ
+                chan ? y
+                SERIAL ! y
 `
 
-new EditorView({
-  doc,
+var cm;
+
+window.onload = function() {
+  cm = new EditorView({
   extensions: [
     basicSetup,
     keymap.of([indentWithTab]),
-    javascript()
   ],
-  parent: document.getElementById("editarea")
-})
-
-
-/*import {basicSetup} from "codemirror"
-import {EditorView, keymap} from "@codemirror/view"
-import {indentWithTab} from "@codemirror/commands"
-
-let exampleCode =
-  `SEQ
-      CHAN OF INT chan:
-      INT x:
-      INT y:
-      x := 0
-      PAR
-          WHILE TRUE
-              SEQ
-                  chan ! x
-                  x := (x+1)
-          WHILE TRUE
-              SEQ
-                  chan ? y
-                  DISPLAY ! y`
-
-
-let editor = new EditorView({
-  exampleCode,
-  extensions: [basicSetup, keymap.of([indentWithTab])],
-  parent: document.getElementById("editarea")
-})
-
-function inserttab(view) {
-  view.dispatch({
-    changes: {from: cm.state.selection.main.head, to: cm.state.selection.main.head, insert: "    "}
+  parent: document.getElementById("editor")
   })
+
+  cm.dispatch({
+    changes: {from: 0, to: 0, insert: exampleCode}
+  })
+
 }
 
-editor.dispatch({
-  changes: {from: 0, to: editor.state.doc.length, insert: exampleCode}
-})
+
+
 
 export function getCode() {
-  return editor.state.doc.toString()
+  return cm.state.doc.toString()
 }
-*/
